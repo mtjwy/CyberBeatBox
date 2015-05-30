595064133973423ab561a5427712fa326dbc541a
Yu Wu <wuyucsu@gmail.com>
2015-05-30 01:23:01 -0500

    MiniMusicApp version 2, takes command-line args

--- CyberBeatBox/src/test/java/com/mtjwy/CyberBeatBox/MiniMiniMusicAppTest.java
+++ CyberBeatBox/src/test/java/com/mtjwy/CyberBeatBox/MiniMiniMusicAppTest.java
@@ -3,6 +3,15 @@ package com.mtjwy.CyberBeatBox;
 public class MiniMiniMusicAppTest {
 	public static void main(String[] args) {
 		MiniMiniMusicApp miniApp = new MiniMiniMusicApp();
-		miniApp.play();
+		//miniApp.play();
+		
+		if (args.length < 2) {
+			System.out.println("Don't forget the instrument and note args");
+			
+		} else {
+			int instrument = Integer.parseInt(args[0]);
+			int note = Integer.parseInt(args[1]);
+			miniApp.play(instrument, note);
+		}
 	}
 }
