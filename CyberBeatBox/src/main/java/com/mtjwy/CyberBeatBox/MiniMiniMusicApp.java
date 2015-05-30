1ac6419cc1aa1adf2c7d9a88d3e771d7c787e1f8
mtjwy <>
2015-05-30 00:58:44 -0500

    play 1 note

---
+++ CyberBeatBox/src/main/java/com/mtjwy/CyberBeatBox/MiniMiniMusicApp.java
@@ -0,0 +1,50 @@
+package com.mtjwy.CyberBeatBox;
+
+import javax.sound.midi.*;
+
+public class MiniMiniMusicApp {
+	public void play() {
+		try {
+			Sequencer player = MidiSystem.getSequencer();
+			player.open();
+			
+			Sequence seq = new Sequence(Sequence.PPQ, 4);
+			
+			Track track = seq.createTrack();
+			
+			//put in some MidiEvents
+			ShortMessage a = new ShortMessage();
+			a.setMessage(144, 1, 44, 100);//put Instruction in the Message,"start playing note 44"
+										//message type, channel, note to play, velocity
+			MidiEvent noteOn = new MidiEvent(a, 1);//trigger a at beat 1
+			track.add(noteOn);
+			
+			ShortMessage b = new ShortMessage();
+			b.setMessage(128, 1, 44, 100);
+			MidiEvent noteOff = new MidiEvent(b, 16);//16: duration
+			track.add(noteOff);
+			
+			player.setSequence(seq);
+			
+			player.start();
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
