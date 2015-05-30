a458c835cd6a286c6ef53472042a0bb7d530bbd3
mtjwy <>
2015-05-29 22:57:40 -0500

    First commit. Great CyberBeatBox Maven project.

---
+++ CyberBeatBox/src/main/java/com/mtjwy/CyberBeatBox/App.java
@@ -0,0 +1,13 @@
+package com.mtjwy.CyberBeatBox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
